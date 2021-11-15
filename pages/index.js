476b64d647d758173f38b1203cd9ba26d8427b52
cemalokten <cemalokten@gmail.com>
2021-11-15 12:56:41 +0000

    Chakra style test
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import { Input, Box, Button, Flex } from '@chakra-ui/react';
 
 export default function Home() {
   return (
@@ -8,21 +9,24 @@ export default function Home() {
       <head>
         <title>Mixlist</title>
       </head>
-      <header>
-        <div>Menu</div>
-        <h1>Mixlist</h1>
-        <button>Search</button>
-      </header>
-      <main>
-        <img src=""></img>
-        <div>Explanation</div>
-        <p>Explore</p>
-        <button>Sign up/ Log in</button>
-      </main>
-      <footer>
-        <p>Contact us</p>
-        <button>Help</button>
-      </footer>
+      <Box w="100%" h="200px" bgGradient="linear(to-r, pink.200, pink.500)">
+        <header>
+          <div>Menu</div>
+          <h1>Mixlist</h1>
+          <button>Search</button>
+        </header>
+        <Input colorScheme="blue" variant="filled" />
+        <Flex direction="row" mb={5} rounded width="500px">
+          <img src=""></img>
+          <div>Explanation</div>
+          <p>Explore</p>
+          <Button colorScheme="blue">Sign up/ Log in</Button>
+        </Flex>
+        <footer>
+          <p>Contact us</p>
+          <button>Help</button>
+        </footer>
+      </Box>
     </>
   );
 }
