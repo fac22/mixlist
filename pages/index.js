7e7edfb6f81f948bb8c5c9314482d1a5b12e30b4
cemalokten <cemalokten@gmail.com>
2021-11-15 16:58:06 +0000

    Tailwind CSS Setup and menu
    - Installed tailwind
    - Converted Index from Chakra to Tailwind
    
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -2,84 +2,47 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import React from 'react';
-import {
-  Input,
-  Box,
-  Button,
-  Flex,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuIcon,
-  MenuCommand,
-  MenuDivider,
-  Heading,
-  UnorderedList,
-  ListItem,
-} from '@chakra-ui/react';
-import {
-  AddIcon,
-  HamburgerIcon,
-  ExternalLinkIcon,
-  RepeatIcon,
-  IconButton,
-  EditIcon,
-  SearchIcon,
-} from '@chakra-ui/icons';
 
 export default function Home() {
   const [toggleMenu, setToggleMenu] = React.useState(true);
+  const menuValues =
+    'flex flex-col w-screen h-screen bg-red-300 fixed justify-center';
+
   return (
     <>
       <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Mixlist</title>
       </head>
-      <Box
-        width="100%"
-        height="100%"
-        bg="red"
-        visibility={toggleMenu ? 'visible' : 'hidden'}
-        position="fixed"
-        paddingTop="40px"
-      >
-        <UnorderedList
-          width="100%"
-          height="100%"
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-around"
-          alignItems="center"
-          listStyleType="none"
-        >
-          <ListItem>HOME</ListItem>
-          <ListItem>HOME</ListItem>
-          <ListItem>HOME</ListItem>
-          <ListItem>HOME</ListItem>
-          <ListItem>HOME</ListItem>
-        </UnorderedList>
-      </Box>
-
-      <Flex direction="row" width="100vw" justifyContent="space-between">
-        <Button
+      <div className={`${menuValues} ${toggleMenu ? 'visible' : 'invisible'}`}>
+        <ul className="w-screen h-3/5 flex flex-col justify-around items-center list-none">
+          <li>HOME</li>
+          <li>PROFILE</li>
+          <li>TREND</li>
+          <li>LOGIN</li>
+        </ul>
+      </div>
+      <div className="w-screen flex flex-row justify-between">
+        <button
           onClick={() =>
             setToggleMenu(() => {
               return toggleMenu === true ? false : true;
             })
           }
         >
-          <HamburgerIcon />
-        </Button>
-        <Heading>Mixlist</Heading>
+          <div className="tham tham-e-squeeze tham-w-6">
+            <div className="tham-box">
+              <div className="tham-inner" />
+            </div>
+          </div>
+        </button>
+        <h1>Mixlist</h1>
 
-        <SearchIcon margin="7px" />
-      </Flex>
+        <span>🔍</span>
+      </div>
+      {/* </Flex>
       <Flex direction="row" mb={5} rounded width="500px">
         <img src=""></img>
         <div>
@@ -89,12 +52,11 @@ export default function Home() {
           </p>
         </div>
         <p>Explore</p>
-        {/* <Button colorScheme="blue">Sign up/ Log in</Button> */}
-      </Flex>
-      <footer>
+      </Flex> */}
+      {/* <footer>
         <p>Contact us</p>
         <button>Help</button>
-      </footer>
+      </footer> */}
     </>
   );
 }
