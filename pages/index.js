75433d30780d0043eddce5986f647d85ae08f361
jihye <jiji.pak41@gmail.com>
2021-11-17 11:39:23 +0000

    create color theme - tailwind, style body and menu
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,53 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import React from 'react';
-import MyDocument from './_document';
 
 export default function Home() {
-  return <MyDocument />;
+  const [toggleMenu, setToggleMenu] = React.useState(true);
+  const menuValues =
+    'flex flex-col w-screen h-screen bg-red-300 fixed justify-center ';
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Mixlist</title>
+      </head>
+
+      <body className="bg-red text-white">
+        <div
+          className={`${menuValues} ${toggleMenu ? 'visible' : 'invisible'}`}
+        >
+          <h1 className="bg-mixlistPurple text-center	">🎸</h1>
+
+          <ul className="w-screen h-screen flex flex-col justify-around items-center list-none bg-mixlistPurple">
+            <li>HOME</li>
+            <li>PROFILE</li>
+            <li>TREND</li>
+            <li>LOGIN</li>
+          </ul>
+        </div>
+        <div className="w-screen flex flex-row justify-between">
+          <button
+            onClick={() =>
+              setToggleMenu(() => {
+                return toggleMenu ? false : true;
+              })
+            }
+          >
+            <div className="tham tham-e-squeeze tham-w-6">
+              <div className="tham-box">
+                <div className="tham-inner" />
+              </div>
+            </div>
+          </button>
+          <h1>Mixlist</h1>
+
+          <span>🔍</span>
+        </div>
+      </body>
+    </html>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  // mode: 'jit', // Add Just In Time Mode
+
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
       mixlistPurple: '#10003D',
+      white: '#FFFFFF',
+      red: '#FFB8A8',
     },
     extend: {},
   },
