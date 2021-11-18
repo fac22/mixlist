7b706ca2c5e9adabb0e1ccba417fb33fcaba6dff
jihye <jiji.pak41@gmail.com>
2021-11-18 17:04:53 +0000

    create pages for all routes
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

---
+++ pages/discover.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+import Profile from '../components/Profile';
+
+import { Header } from '../components/Header';
+
+import Menu from '../components/Menu';
+import Search from '../components/Search';
+import { BackgroundWrapper } from '../components/BackgroundWrapper';
+
+export default function Discover() {
+  return <h1 className="text-WHITE">Discover</h1>;
+}
