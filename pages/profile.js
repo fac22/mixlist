8b4f6a2e42fa74dd64b2086ef1f5fb26d19d1a0e
jihye <jiji.pak41@gmail.com>
2021-11-18 12:19:42 +0000

    pass props to each component
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- pages/profile.js
+++ pages/profile.js
@@ -6,13 +6,20 @@ import RecReviews from '../components/RecReviews';
 import RecLogged from '../components/RecLogged';
 
 export default function Profile() {
+  const colours = {
+    text: 'WHITE',
+    username: 'BLUEPRO',
+    favtitle: 'PINKPRO',
+    reviewtitle: 'PURPLEPRO',
+    loggedtitle: 'GREENPRO',
+  };
   return (
     <main className="flex flex-col bg-PURPLET text-WHITE">
       <Hamburger />
-      <UserInfo />
-      <FavAlbums />
-      <RecReviews />
-      <RecLogged />
+      <UserInfo userTitleColour={colours.username} />
+      <FavAlbums favTitleColour={colours.favtitle} />
+      <RecReviews reviewTitleColour={colours.reviewtitle} />
+      <RecLogged loggedTitleColour={colours.loggedtitle} />
     </main>
   );
 }
