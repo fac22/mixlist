7794b29c1e6aef26192c0fde74df19db0050fb74
jihye <jiji.pak41@gmail.com>
2021-11-18 12:21:02 +0000

    style logged component - colour, alignment
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- components/RecLogged.js
+++ components/RecLogged.js
@@ -1,29 +1,40 @@
-export default function RecLogged() {
+export default function RecLogged(props) {
+  const albumImgs = [
+    {
+      id: 1,
+      src: 'https://lastfm.freetls.fastly.net/i/u/300x300/263880589b66186a82acd18187920c0c.png',
+    },
+    {
+      id: 2,
+
+      src: 'https://lastfm.freetls.fastly.net/i/u/34s/3b54885952161aaea4ce2965b2db1638.png',
+    },
+    {
+      id: 3,
+
+      src: 'https://lastfm.freetls.fastly.net/i/u/300x300/190429befa66419db3510a86774006cb.png',
+    },
+    {
+      id: 4,
+
+      src: 'https://lastfm.freetls.fastly.net/i/u/300x300/df7a337e5a38415db87b5a9f8020806c.png',
+    },
+  ];
   return (
-    <>
-      <p>Recently Logged</p>
-      <div className="grid grid-cols-2 sm:grid-cols-4">
-        <img
-          src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR1fS_2x5rxyCW_TvpIGEEYtA8XtEGW7C1hbWtEh9jQWn_Jai2uxim3SR4KRIjo-3psR9G24_HmDuQ-cxFJBtUN9D3qWstPLw&usqp=CAc"
-          className=""
-          alt=""
-        ></img>
-        <img
-          src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR1fS_2x5rxyCW_TvpIGEEYtA8XtEGW7C1hbWtEh9jQWn_Jai2uxim3SR4KRIjo-3psR9G24_HmDuQ-cxFJBtUN9D3qWstPLw&usqp=CAc"
-          className=""
-          alt=""
-        ></img>
-        <img
-          src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR1fS_2x5rxyCW_TvpIGEEYtA8XtEGW7C1hbWtEh9jQWn_Jai2uxim3SR4KRIjo-3psR9G24_HmDuQ-cxFJBtUN9D3qWstPLw&usqp=CAc"
-          className=""
-          alt=""
-        ></img>
-        <img
-          src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcR1fS_2x5rxyCW_TvpIGEEYtA8XtEGW7C1hbWtEh9jQWn_Jai2uxim3SR4KRIjo-3psR9G24_HmDuQ-cxFJBtUN9D3qWstPLw&usqp=CAc"
-          className=""
-          alt=""
-        ></img>
+    <div className="">
+      <p className={`text-${props.loggedTitleColour} m-5 text-lg`}>
+        RECENTLY LOGGED
+      </p>
+      <div className="grid grid-cols-3 sm:grid-cols-3 m-auto">
+        {albumImgs.map((item) => (
+          <img
+            src={item.src}
+            alt=""
+            key={item.id}
+            className="w-3/4 m-auto mb-3"
+          ></img>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
