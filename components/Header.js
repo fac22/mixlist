5a02b14f8ac3cdd6ea93d0d2df38133cc46bd0f1
jihye <jiji.pak41@gmail.com>
2021-11-19 11:38:05 +0000

    change search icon to profile icon and link it to the profile page
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,13 @@ import Image from 'next/image';
 import { HiOutlineMenu } from 'react-icons/hi';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { MdOutlineClose } from 'react-icons/md';
+import { CgProfile } from 'react-icons/cg';
 
 export const Header = ({
   toggleMenu,
   setToggleMenu,
-  toggleSearch,
-  setToggleSearch,
+  toggleProfile,
+  setToggleProfile,
   content,
   textColor,
 }) => {
@@ -15,23 +16,23 @@ export const Header = ({
     setToggleMenu(() => (toggleMenu === true ? false : true));
   };
 
-  const toggleSearchFn = () => {
-    setToggleSearch(() => (toggleSearch === true ? false : true));
+  const toggleProfileFn = () => {
+    setToggleProfile(() => (toggleProfile === true ? false : true));
   };
 
-  const searchJustify = toggleSearch ? 'justify-end' : 'justify-between';
+  const profileJustify = toggleProfile ? 'justify-end' : 'justify-between';
   const menuToggle =
-    toggleMenu || toggleSearch
+    toggleMenu || toggleProfile
       ? 'z-30 bg-none'
       : 'visible bg-DPURPLE bg-opacity-95';
   const toggleCloseButton = toggleMenu ? 'visible' : 'hidden';
-  const searchToggle = toggleSearch ? 'visible' : 'hidden';
-  const hideMenuButton = toggleMenu || toggleSearch ? 'hidden' : 'visible';
+  const profileToggle = toggleProfile ? 'visible' : 'hidden';
+  const hideMenuButton = toggleMenu || toggleProfile ? 'hidden' : 'visible';
   const iconStyle = 'text-WHITE fill-current';
 
   return (
     <div
-      className={`flex flex-row w-full p-5 fixed z-30 ${menuToggle} ${searchJustify}`}
+      className={`flex flex-row w-full p-5 fixed z-30 ${menuToggle} ${profileJustify}`}
     >
       {/* Left Menu Buttons*/}
       <button onClick={toggleMenuFn} className={`${hideMenuButton}`}>
@@ -48,8 +49,8 @@ export const Header = ({
 
       {/* Right Close Button */}
       <button
-        onClick={() => setToggleSearch(toggleSearchFn)}
-        className={`${searchToggle}`}
+        onClick={() => setToggleProfile(toggleProfileFn)}
+        className={`${profileToggle}`}
       >
         <MdOutlineClose
           className={`${iconStyle} transform translate-y-px self-end`}
@@ -65,12 +66,9 @@ export const Header = ({
       </h2>
 
       {/* Right Search Button */}
-      <button
-        onClick={() => setToggleSearch(toggleSearchFn)}
-        className={`${hideMenuButton}`}
-      >
-        <AiOutlineSearch className={iconStyle} size={24} />
-      </button>
+      <a href={'/profile'} className={`${hideMenuButton}`}>
+        <CgProfile className={iconStyle} size={24} />
+      </a>
     </div>
   );
 };
