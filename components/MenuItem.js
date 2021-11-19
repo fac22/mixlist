14998ed5a3c6fbdeffa17476f2b107f6b321336a
cemalokten <cemalokten@gmail.com>
2021-11-19 10:17:54 +0000

    Next/Link Router
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- components/MenuItem.js
+++ components/MenuItem.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 export default function MenuItem(props) {
   return (
     <li
       className={`text-${props.tCol} cursor-pointer font-header text-4xl tracking-widest text-center mb-8 z-20`}
     >
-      <a href={props.href}>{props.title}</a>
+      <Link href={props.href}>
+        <a>{props.title}</a>
+      </Link>
     </li>
   );
 }
