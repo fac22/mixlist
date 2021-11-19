17a54a65c559ff7fe9c9e0ee2df6c19050731d4a
MJOW1999 <michael.jow.williams@gmail.com>
2021-11-19 10:56:46 +0000

    changed styles of artist routes to make it visible

--- pages/artists/[name].js
+++ pages/artists/[name].js
@@ -1,11 +1,12 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 
-export default function ArtistDetails() {
+export default function Details() {
   const router = useRouter();
   const artist = router.query.name;
 
   const [art, setArt] = React.useState();
+  const [ta, setTa] = React.useState();
   React.useEffect(() => {
     fetch(
       `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=e836ddbce95921744c7e9efe110bcd54&format=json`
@@ -16,15 +17,32 @@ export default function ArtistDetails() {
       });
   }, [artist]);
 
-  console.log(art);
+  React.useEffect(() => {
+    fetch(
+      `https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&api_key=e836ddbce95921744c7e9efe110bcd54&format=json`
+    )
+      .then((a) => a.json())
+      .then((data) => {
+        console.log(data);
+        setTa(data.topalbums.album);
+      });
+  }, [artist]);
+  console.log(ta);
 
-  if (!art) return <div>Loading...</div>;
+  if (!art || !ta) return <div>Loading...</div>;
   return (
     <>
-      <div>
+      <div className="text-WHITE align-center w-full p-5">
         <h1>{art.name} page</h1>
         <img src={`${art.image[1][`#text`]}`} alt="" />
         <p>{art.bio.summary.split('.')[0] + '.'}</p>
+        <h2>Top Albums</h2>
+        {ta.slice(0, 10).map((e) => (
+          <div key={e.mbid}>
+            <li>{e.name}</li>
+            <img src={`${e.image[1][`#text`]}`} />
+          </div>
+        ))}
       </div>
     </>
   );
