1d9e61cff28c468f3e7bb26fc6a5f869c8650d0a
MJOW1999 <michael.jow.williams@gmail.com>
2021-11-22 14:04:39 +0000

    Supabase user auth with magic link
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>

---
+++ pages/index1.js
@@ -0,0 +1,86 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+import { Button } from '../components/Button';
+import { Header } from '../components/Header';
+import { Main } from '../components/Main';
+import { H2 } from '../components/H2';
+import Input from '../components/Input';
+import Menu from '../components/Menu';
+import Search from '../components/Search';
+import { BackgroundWrapper } from '../components/BackgroundWrapper';
+import { motion } from 'framer-motion';
+import Layout from '../components/Layout';
+
+const settings = {
+  initial: {
+    opacity: 1,
+    x: 250,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+
+export default function Home() {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [toggleSearch, setToggleSearch] = React.useState(false);
+  return (
+    <motion.div initial={'initial'} animate={'animate'} exit={{ opacity: 0 }}>
+      <Head>
+        <Layout title="MIXLIST" />
+      </Head>
+
+      <Menu toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
+      <Search toggleSearch={toggleSearch} setToggleSearch={setToggleSearch} />
+      <Header
+        toggleMenu={toggleMenu}
+        setToggleMenu={setToggleMenu}
+        toggleSearch={toggleSearch}
+        setToggleSearch={setToggleSearch}
+        textColor={'WHITE'}
+        content={'MIXLIST'}
+      />
+      <BackgroundWrapper toggleMenu={toggleMenu} toggleSearch={toggleSearch}>
+        <motion.div variants={settings}>
+          <Main toggleMenu={toggleMenu} toggleSearch={toggleSearch}>
+            <section className="mt-28 mb-20">
+              <H2 color={'GREENT'}>DISCOVER</H2>
+              <H2 color={'BLUET'}>YOUR NEXT</H2>
+              <H2 color={'PINK'}>FAVOURITE</H2>
+              <H2 color={'PINKT'}>ALBUM</H2>
+            </section>
+
+            <section className="flex flex-col items-center justify-center">
+              <Input
+                placeHolder="EMAIL"
+                bgColor="REDT"
+                textColor="DRED"
+                bgColorHover={'REDHOVER'}
+                type={'email'}
+              />
+              <Input
+                placeHolder="PASSWORD"
+                bgColor="BLUET"
+                textColor="DBLUE"
+                bgColorHover={'BLUEHOVER'}
+                type={'password'}
+              />
+              <Button
+                bgColor={'GREENT'}
+                textColor={'DGREEN'}
+                bgColorHover={'GREENHOVER'}
+                title={'SIGN UP'}
+                route={'login'}
+              />
+            </section>
+          </Main>
+        </motion.div>
+      </BackgroundWrapper>
+    </motion.div>
+  );
+}
