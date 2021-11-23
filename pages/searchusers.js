2d029caf110d2e99f455dd23dc825e2522819689
cemalokten <cemalokten@gmail.com>
2021-11-23 15:22:06 +0000

    Search function (80% Complete)
    - [x] Search Works
    - [x] Gives correct error when no user found
    
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- pages/searchusers.js
+++ pages/searchusers.js
@@ -1,52 +1,50 @@
 import { Button } from '../components/Button';
 import { supabase } from '../utils/supabaseClient';
 import { useState, useEffect, createRef, useRef } from 'react';
+import { useFormik } from 'formik';
 
 export default function SearchUsers() {
   const [username, setUsername] = useState('SASHA');
-  const inputRef = createRef();
 
-  async function getUser() {
-    const { data, error } = await supabase
-      .from('profiles')
-      .select()
-      .textSearch('username', username)
-      .single();
-    setUsername(data.username);
+  async function getUser(userSearch) {
+    try {
+      const { data, error } = await supabase
+        .from('profiles')
+        .select()
+        .textSearch('username', userSearch)
+        .single();
+      setUsername(data);
+    } catch {
+      console.log('USER NOT FOUND');
+    }
   }
 
   useEffect(() => {
-    getUser();
-    console.log(username);
+    console.log(username.profile_img);
   }, [username]);
+
+  const formik = useFormik({
+    initialValues: {
+      search: '',
+    },
+    onSubmit: (values) => {
+      getUser(values.search);
+    },
+  });
 
   return (
     <>
       <section className="flex flex-col items-center justify-center">
-        <form>
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="search">Search</label>
           <input
-            placeholder="TYPE USERNAME"
-            bgcolor="REDT"
-            textcolor="DRED"
-            bgcolorhover={'REDHOVER'}
-            type={'text'}
-            onSubmit={(e) => {
-              e.preventDefault();
-              setUsername(e.target.value);
-            }}
+            id="search"
+            name="search"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.search}
           />
-
-          <button
-            bgcolor={'GREENT'}
-            textcolor={'DGREEN'}
-            bgcolorhover={'GREENHOVER'}
-            title={'SEARCH'}
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-          >
-            SEARCH
-          </button>
+          <button type="submit">Submit</button>
         </form>
       </section>
     </>
