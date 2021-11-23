40011ec591cfd6370769616c19b6f42dd1ae782b
jihye <jiji.pak41@gmail.com>
2021-11-23 22:45:17 +0000

    remove arrays for img sources trying to connect and  use database
    Co-authored-by: cemalokten <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- components/RecReviews.js
+++ components/RecReviews.js
@@ -2,35 +2,13 @@ import { H3 } from './H3';
 import { Review } from './Review';
 import Image from 'next/image';
 
-export default function RecReviews({ userReview }) {
-  const albumImgs = [
-    {
-      id: 1,
-      src: 'https://lastfm.freetls.fastly.net/i/u/300x300/263880589b66186a82acd18187920c0c.png',
-    },
-    {
-      id: 2,
-
-      src: 'https://lastfm.freetls.fastly.net/i/u/34s/3b54885952161aaea4ce2965b2db1638.png',
-    },
-    {
-      id: 3,
-
-      src: 'https://lastfm.freetls.fastly.net/i/u/300x300/190429befa66419db3510a86774006cb.png',
-    },
-    {
-      id: 4,
-
-      src: 'https://lastfm.freetls.fastly.net/i/u/300x300/df7a337e5a38415db87b5a9f8020806c.png',
-    },
-  ];
+export default function RecReviews(props) {
+  const albumImgs = [];
   return (
     <div className="mt-4">
       <H3 color="BLUE">LATEST REVIEWS</H3>
       <ul className=" flex flex-col">
         <li className="sm:w-4/5 flex flex-col">
-          {}
-
           {albumImgs.slice(0, 2).map((item) => (
             <div className={`flex flex-row mt-2 mb-2`} key={props.id}>
               <Review
