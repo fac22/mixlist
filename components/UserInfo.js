5833719cec94b30f0c29c28072195085178e24a5
cemalokten <cemalokten@gmail.com>
2021-11-23 12:54:58 +0000

    Database to user profile
    <60609268+cemalokten@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Sasha <59370143+cerealenjoyer@users.noreply.github.com>

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,19 +1,14 @@
 import Image from 'next/image';
 
-export default function UserInfo(props) {
+export default function UserInfo({ username, imageURL, userTitleColour }) {
   return (
     <div className=" flex flex-row">
       <div className="mr-4">
-        <Image
-          src="blob:http://localhost:3000/79f9a1c8-e8a4-448e-9bc4-f1fc9997be51"
-          width={100}
-          height={100}
-          alt=""
-        ></Image>
+        <Image src={imageURL} width={100} height={100} alt=""></Image>
       </div>
       <ul className="font-body">
-        <li className={`text-${props.userTitleColour}`}>
-          <span className="text-GREENT block">USERNAME</span>
+        <li className={`text-${userTitleColour}`}>
+          <span className="text-GREENT block">{username}</span>
         </li>
         <li className="text-xs">52 Reviews</li>
         <li className="text-xs">3120 Albums in collection</li>
--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import Profile from '../components/Profile';
+import { useState, useEffect } from 'react';
+import { supabase } from '../utils/supabaseClient';
+import Avatar from '../components/Avatar';
+import getProfile from '../utils/getProfile';
 
 import { Header } from '../components/Header';
 
@@ -10,8 +14,32 @@ import { BackgroundWrapper } from '../components/BackgroundWrapper';
 import Layout from '../components/Layout';
 
 export default function ProfilePage() {
-  const [toggleMenu, setToggleMenu] = React.useState(false);
-  const [toggleSearch, setToggleSearch] = React.useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleSearch, setToggleSearch] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState(null);
+  const [profile_img, setProfileImg] = useState(null);
+
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  const setStateFunctions = {
+    setUsername,
+    setProfileImg,
+    setLoading,
+  };
+
+  useEffect(() => {
+    getProfile(setStateFunctions);
+  }, [session]);
+
   return (
     <>
       <Head>
@@ -28,7 +56,7 @@ export default function ProfilePage() {
         content={'MIXLIST'}
       />
       <BackgroundWrapper toggleMenu={toggleMenu} toggleSearch={toggleSearch}>
-        <Profile />
+        <Profile username={username} imageURL={profile_img} />
       </BackgroundWrapper>
     </>
   );
