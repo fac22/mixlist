9ca184c2abb40cee77fc172d6c85c5fde0271bda
MJOW1999 <michael.jow.williams@gmail.com>
2021-11-24 16:47:51 +0000

    Album page styled

--- pages/artists/[name]/[album].js
+++ pages/artists/[name]/[album].js
@@ -1,9 +1,38 @@
 import { useRouter } from 'next/router';
-import React from 'react';
-import AlbumResults from '../../../components/AlbumSearchResults';
-import Search from '../../../components/AlbumSearch';
 import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+// import { Button } from '../components/Button';
+// import { Header } from '../components/Header';
+// import { Main } from '../components/Main';
+// import { H2 } from '../components/H2';
+// import { H3 } from '../components/H3';
+// import Input from '../components/Input';
+// import album from '../public/album.jpg';
+// import Menu from '../components/Menu';
+// import Search from '../components/Search';
+// import { P } from '../components/P';
+// import { BackgroundWrapper } from '../components/BackgroundWrapper';
+// import { BiLike } from 'react-icons/bi';
+// import { BiPlus } from 'react-icons/bi';
+// import { Review } from '../components/Review';
+// import InputTextArea from '../components/InputTextArea';
+import Layout from '../../../components/Layout';
+import Search from '../../../components/Search';
+import { Header } from '../../../components/Header';
+import { BackgroundWrapper } from '../../../components/BackgroundWrapper';
+import { Main } from '../../../components/Main';
+import Menu from '../../../components/Menu';
+import { H3 } from '../../../components/H3';
+import { BiLike } from 'react-icons/bi';
+import { BiPlus } from 'react-icons/bi';
+import { Button } from '../../../components/Button';
+import InputTextArea from '../../../components/InputTextArea';
+
 const ArtistAlbum = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [toggleSearch, setToggleSearch] = React.useState(false);
   const router = useRouter();
   console.log(router.query);
   const { name, album } = router.query;
@@ -29,22 +58,81 @@ const ArtistAlbum = () => {
 
   return (
     <>
-      <div className="text-WHITE align-center w-full h-full p-20">
+      <Head>
+        <Layout title={name} />
+      </Head>
+      <Menu toggleMenu={toggleMenu} setToggleMenu={setToggleMenu} />
+      <Search toggleSearch={toggleSearch} setToggleSearch={setToggleSearch} />
+      <Header
+        toggleMenu={toggleMenu}
+        setToggleMenu={setToggleMenu}
+        toggleSearch={toggleSearch}
+        setToggleSearch={setToggleSearch}
+        textColor={'WHITE'}
+        content={'MIXLIST'}
+      />
+      <BackgroundWrapper toggleMenu={toggleMenu} toggleSearch={toggleSearch}>
+        <Main toggleMenu={toggleMenu} toggleSearch={toggleSearch}>
+          <section className="mt-14 mb-2">
+            <Image
+              src={`${albInfo.image[4][`#text`]}`}
+              alt=""
+              width="335"
+              height="335"
+            />
+          </section>
+          <section>
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-col">
+                <H3 color={'PINKT'}>{album} </H3>
+                <H3 color={'BLUET'}>by {name}</H3>
+              </div>
+              <div className="flex flex-row items-start">
+                <H3 color={'WHITE'}>
+                  <BiLike size={22} className="mr-2" />
+                </H3>
+                <H3 color={'WHITE'}>
+                  <BiPlus size={26} />
+                </H3>
+              </div>
+            </div>
+          </section>
+          <section className="divide-dotted divide-BLUET grid grid-cols-1 divide-y">
+            <article className="pt-4">
+              <H3 color={'PINKT'}>ADD REVIEW</H3>
+              <InputTextArea
+                placeHolder="YOUR REVIEW HERE..."
+                bgColor="PINKT"
+                textColor="DPINK"
+                bgColorHover={'PINKHOVER'}
+                type={'textarea'}
+              />
+              <Button
+                bgColor="PINKT"
+                textColor="DPINK"
+                bgColorHover={'PINKHOVER'}
+                title={'REVIEW'}
+              />
+            </article>
+          </section>
+        </Main>
+      </BackgroundWrapper>
+      {/* <div className="text-WHITE align-center w-full h-full p-20">
         <img src={`${albInfo.image[4][`#text`]}`} alt="" />
-        <h1>{name}</h1>
         <h1>{album}</h1>
-        {/* {albInfo.wiki.published ? (
+        <h1>by {name}</h1>
+        {albInfo.wiki.published ? (
           <h2>Released: {albInfo.wiki.published.split(',')[0]}</h2>
         ) : (
           ''
-        )} */}
+        )}
 
-        {/* {albInfo.tags.tag.map((song) => {
+        {albInfo.tags.tag.map((song) => {
           <li>{song.name}</li>;
-        })} */}
+        })}
 
-        {/* <span>{albInfo.tracks.track[0].name}</span> */}
-      </div>
+        <span>{albInfo.tracks.track[0].name}</span>
+      </div> */}
     </>
   );
 };
