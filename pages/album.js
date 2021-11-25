8ff9ae70b2d6749d772a1e1b949cfb4057e2f7cd
MJOW1999 <michael.jow.williams@gmail.com>
2021-11-25 18:17:04 +0000

    Build fix - changed Review to ReviewComp in album.js
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>

--- pages/album.js
+++ pages/album.js
@@ -17,6 +17,7 @@ import { BiPlus } from 'react-icons/bi';
 import { Review } from '../components/Review';
 import InputTextArea from '../components/InputTextArea';
 import Layout from '../components/Layout';
+import { ReviewComp } from '../components/ReviewComp';
 
 export default function Home() {
   const [toggleMenu, setToggleMenu] = React.useState(false);
@@ -60,20 +61,24 @@ export default function Home() {
           </section>
 
           <section className="divide-dotted divide-BLUET grid grid-cols-1 divide-y">
-            <Review image={album} alt={'Album Art'} username={'LEVEL87 (3123)'}>
+            <ReviewComp
+              image={album}
+              alt={'Album Art'}
+              username={'LEVEL87 (3123)'}
+            >
               Something in this album is far more beautiful than sounds should
               be able to convey. The album that accompanied the deepest and most
               profound moments of my lockdown. Favorite track: Something In The
               Sadness.
-            </Review>
-            <Review image={album} alt={'Album Art'} username={'HELLO123'}>
+            </ReviewComp>
+            <ReviewComp image={album} alt={'Album Art'} username={'HELLO123'}>
               Something in this album is far more beautiful than sounds should
               be able to convey. Sadness.
-            </Review>
-            <Review image={album} alt={'Album Art'} username={'HELLO123'}>
+            </ReviewComp>
+            <ReviewComp image={album} alt={'Album Art'} username={'HELLO123'}>
               Something in this album is far more beautiful than sounds should
               be able to convey. Favorite track: Something In The Sadness.
-            </Review>
+            </ReviewComp>
             <article className="pt-4">
               <H3 color={'PINKT'}>ADD REVIEW</H3>
               <InputTextArea
